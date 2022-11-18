b0c311dfcbc736ff81e1320e3891ce1439ad0719
rodionion <rodionvh@gmail.com>
2022-11-18 09:20:01 +0200

    Set up webpack

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: "development",
+	entry: {
+		index: './src/index.js',
+	},
+    devServer: {
+        static: "./dist",
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+			title: 'My To-Do App',
+			template: "./src/index.html"
+		})
+    ],
+    module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+            {
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: 'asset/resource',
+			},
+		],
+	},
+	output: {
+		filename: '[name].bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+        assetModuleFilename: '[name][ext]',
+        clean: true,
+	},
+    optimization: {
+        runtimeChunk: "single",
+    },
+};
